e34f4a5a2e9f9aff8658e5c62655a713cdbfc1a8
周晓东 <xiaodong.zhou@tingjiandan.com>
2022-08-09 21:20:25 +0800

    run.sh

--- rcfile/run.sh
+++ rcfile/run.sh
@@ -12,7 +12,7 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 cp ~/item/rcfile/zsh/zshrc ~/.zshrc
 
-mv ~/item/rcfile/zsh/bira.zsh-theme ~/.oh-my-zsh/themes/bira.zsh-theme
+cp -f  ~/item/rcfile/zsh/bira.zsh-theme ~/.oh-my-zsh/themes/bira.zsh-theme
 
 git clone https://github.com/joelthelion/autojump.git ~/autojump
 
