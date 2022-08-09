c92099a97aefa1fc26f8b6184fdb841e139ecdd7
周晓东 <xiaodong.zhou@tingjiandan.com>
2022-08-09 21:09:59 +0800

    添加autojump支持

--- rcfile/run.sh
+++ rcfile/run.sh
@@ -8,11 +8,22 @@ vim ~/a.txt <<EOF
 :wq
 EOF
 
-git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 cp ~/item/rcfile/zsh/zshrc ~/.zshrc
 
 mv ~/item/rcfile/zsh/bira.zsh-theme ~/.oh-my-zsh/themes/bira.zsh-theme
+
+git clone https://github.com/joelthelion/autojump.git ~/autojump
+
+~/autojump/install.py
+
+
+
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 chsh -s /bin/zsh <<EOF
 uc.0079aml
